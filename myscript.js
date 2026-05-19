f0a1d3ac5ff042c33693da71d333e35ee323da52
Eren Akyıldız <32164674+CuriousandLazy@users.noreply.github.com>
2026-05-19 09:23:01 +0300

    Basic ui implemented

--- myscript.js
+++ myscript.js
@@ -13,13 +13,12 @@ function getHumanChoice(choices){
         return getHumanChoice(choices);
     }
 }
-function playRound(choices){
-    humanChoice = getHumanChoice(choices);
+function playRound(humanChoice){
     computerChoice = getComputerChoice(choices);
     if(humanChoice == computerChoice){
-        console.log(`Its a draw`)
-        console.log(`You both chose ${humanChoice}`);
-        console.log(`Current Score - You: ${humanScore}, Computer: ${computerScore}`);
+        document.getElementById('results').textContent = `It's a draw! You both chose ${humanChoice}`;
+        document.getElementById('human-score').textContent = humanScore;
+        document.getElementById('computer-score').textContent = computerScore;
         return;
     }
 
@@ -27,20 +26,18 @@ function playRound(choices){
         (humanChoice == "paper" && computerChoice == "rock") || 
         (humanChoice == "scissors" && computerChoice == "paper")){
         humanScore++;
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        console.log(`Current Score - You: ${humanScore}, Computer: ${computerScore}`);
+        document.getElementById('human-score').textContent = humanScore;
+        document.getElementById('results').textContent = `You win! ${humanChoice} beats ${computerChoice}`;
     }
     // Handle the cases where the computer wins
     else{
         computerScore++;
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        console.log(`Current Score - You: ${humanScore}, Computer: ${computerScore}`);
+        document.getElementById('results').textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+        document.getElementById('computer-score').textContent = computerScore;
     }
-    }
+}
 function playGame(choices){
-    for(let i = 0; i < 5; i++){
-        playRound(choices);
-    }
+
     console.log(`Final Score - You: ${humanScore}, Computer: ${computerScore}`);
 }
 
@@ -49,4 +46,3 @@ let computerScore = 0;
 const choices = ["rock","paper","scissors"];
 console.log("Let's play Rock, Paper, Scissors!");
 playGame(choices);
-
